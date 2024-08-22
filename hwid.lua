9bfc8bc4929dd8834cdd1eb7ddd4d5194f13bf76
NervigeMuecke <139649304+NervigeMuecke@users.noreply.github.com>
2024-08-22 20:10:03 +0200

    Update hwid.lua

--- hwid.lua
+++ hwid.lua
@@ -3,7 +3,7 @@ local function IsHWIDAllowed(hwid)
         "5EE1F134-B660-434F-A6C6-3A825069B8D2", --> Owner
         "85AE86FF-A1F4-48B3-BB91-357366878846", --> Kamani
         "1607C69B-69D4-4693-BCA8-D900241484D5", --> Dark Vex
-        "77F713D3-730A-4E09-A97D-150DA2B30079", --> Obibob
+        "8225BC76-3A18-4231-87A8-6D86F92B61E0", --> Obibob
         -- Some are incorrect, need to renew them
     }
     return table.find(allowedHWIDs, hwid) ~= nil
