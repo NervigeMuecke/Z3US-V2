1da4a2ad38e7ec1e6862f8801f866a04a62e2b2d
NervigeMuecke <139649304+NervigeMuecke@users.noreply.github.com>
2024-09-16 19:18:03 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,18 +1,51 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/Library.lua"))()
+wait(2)
 
-local Repository = "https://raw.githubusercontent.com/NervigeMuecke/Z3US-V2/main/"
+local GameId = game.PlaceId
 
-local SupportedGames = {
-    [17625359962] = Repository .. "Z3US%20Rivals.lua",    -- Rivals
-    [286090429] = Repository .. "Z3US%20V2.5%20ARSENAL",    -- Arsenal
-    [5938036553] = Repository .. "Z3US%20Frontlines%20V1.0.0", -- Frontlines
-}
+local function ExecuteRivals()
+    Library:Notify("Z3US has detected you being in Rivals.", 5)
+    wait(5)
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/NervigeMuecke/Z3US-V2/main/Games/Z3US%20Rivals.lua'))()
+end
 
-local Game = SupportedGames[game.PlaceId]
+local function ExecuteFootball()
+    Library:Notify("Z3US has detected you being in a Touch Football.", 5)
+    wait(5)
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/NervigeMuecke/Z3US-V2/main/Games/Touch%20Football%20Z3US.lua'))()
+end
 
-if not Game then
-    Library:Notify("You are not in a supported game, loading universal..")
+local function ExecuteArsenal()
+    Library:Notify("Z3US has detected you being in Arsenal.", 5)
+    wait(5)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NervigeMuecke/Z3US-V2/main/Games/Z3US%20V2.5%20ARSENAL"))()
+end
+
+local function ExecuteFront()
+    Library:Notify("Z3US has detected you being in Frontlines.", 5)
+    wait(5)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NervigeMuecke/Z3US-V2/main/Games/Z3US%20Frontlines%20V1.0.0"))()
+end
+
+local function ExecuteUniversal()
+    Library:Notify("You are not in a supported game, loading universal..", 5)
+    wait(5)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/NervigeMuecke/Z3US-V2/main/Games/Z3US%20UNIVERSAL%20BETA%20V2.2"))()
 end
 
-loadstring(game:HttpGet(Game))()
+
+local function ExecuteGame()
+    if GameId == 16447934574 then 
+        ExecuteFootball()
+    elseif GameId == 286090429 then
+        ExecuteArsenal()
+    elseif GameId == 17625359962 then
+        ExecuteRivals()
+    elseif GameId == 5938036553 then
+        ExecuteFront()
+    else
+        ExecuteUniversal()
+    end
+end
+
+ExecuteGame()
