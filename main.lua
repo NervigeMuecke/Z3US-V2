900f336d9da1f5833d26fc20f20de47838a6a0b8
NervigeMuecke <139649304+NervigeMuecke@users.noreply.github.com>
2024-10-03 19:06:57 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,7 +5,7 @@ local RunService = game:GetService("RunService")
 
 local player = game.Players.LocalPlayer
 local accage = player.AccountAge
-local age = 365
+local age = 150
 
 if accage > age then
     player:Kick("You got detected using a main acc, I don't support cheating on main accs. Use an alt >:(")
