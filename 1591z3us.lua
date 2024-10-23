56a3bf41ba67cccda299ee85dd7166bc3aef9e72
NervigeMuecke <139649304+NervigeMuecke@users.noreply.github.com>
2024-10-23 21:31:14 +0200

    Update 1591z3us.lua

--- 1591z3us.lua
+++ 1591z3us.lua
@@ -13,7 +13,7 @@ end
 warummacheichdas()
 wait(1)
 
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/Library.lua"))()
+--local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/Library.lua"))()
 local GameId = game.PlaceId
 
 local RunService = game:GetService("RunService")
@@ -27,31 +27,31 @@ if accage > age then
 end
 
 local function ExecuteRivals()
-    Library:Notify("Z3US has detected you being in Rivals.", 5)
+    --Library:Notify("Z3US has detected you being in Rivals.", 5)
     wait(5)
     loadstring(game:HttpGet('https://raw.githubusercontent.com/NervigeMuecke/Z3US-V2/main/Games/Z3US%20Rivals.lua'))()
 end
 
 local function ExecuteFootball()
-    Library:Notify("Z3US has detected you being in a Touch Football.", 5)
+    --Library:Notify("Z3US has detected you being in a Touch Football.", 5)
     wait(5)
     loadstring(game:HttpGet('https://raw.githubusercontent.com/NervigeMuecke/Z3US-V2/main/Games/Touch%20Football%20Z3US.lua'))()
 end
 
 local function ExecuteArsenal()
-    Library:Notify("Z3US has detected you being in Arsenal.", 5)
+    --Library:Notify("Z3US has detected you being in Arsenal.", 5)
     wait(5)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/NervigeMuecke/Z3US-V2/refs/heads/main/Games/Z3US%20Arsenal%20V3.lua"))()
 end
 
 local function ExecuteFront()
-    Library:Notify("Z3US has detected you being in Frontlines.", 5)
+    --Library:Notify("Z3US has detected you being in Frontlines.", 5)
     wait(5)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/NervigeMuecke/Z3US-V2/main/Games/Z3US%20Frontlines%20V1.0.0"))()
 end
 
 local function ExecuteUniversal()
-    Library:Notify("You are not in a supported game, loading universal..", 5)
+    --Library:Notify("You are not in a supported game, loading universal..", 5)
     wait(5)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/NervigeMuecke/Z3US-V2/main/Games/Z3US%20UNIVERSAL%20BETA%20V2.2"))()
 end
